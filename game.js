0618672667661c377b781a7e1cf1462c3fcbf9c1
TPlemur <TPlemur@gmail.com>
2022-10-12 15:48:08 -0700

    added dvd multi-sprite

--- game.js
+++ game.js
@@ -1,13 +1,87 @@
-title = "FooBar";
+title = "DVD";
 
 description = ``;
 
-characters = [];
+characters = [
+`
+      
+   lll
+ lllll
+ l    
+ll lll
+ll lll
+`,
+`
+ lllll
+llllll
+llllll
+ll lll
+ l lll
+ l lll
+`,
+`
+ll    
+llllll
+llllll
+ l    
+ l lll
+ l lll
+`,
+`
+   
+   
+ll 
+ll 
+ ll
+ ll
+`,
+`
+ll lll
+ l
+ lllll
+   lll
+      
+`,
+`
+ ll l 
+llll l
+llllll
+llllll
+ lllll
+`,
+`
+ll lll
+ll    
+llllll
+llllll
+ll    
+`,
+`
+ ll
+ll 
+ll 
+   
+   
+`
+];
 
 const G ={
    WIDTH: 150,
    HEIGHT: 150, 
+   BPM: 80 //BPM of song - change as necessasary
 };
+
+dvdLogo = {
+    pos: vec(G.WIDTH/2,G.HEIGHT/2),
+    aPos: vec(-7,-3),
+    bPos: vec(-1,-3),
+    cPos: vec(5,-3),
+    dPos: vec(9,-3),
+    ePos: vec(-7,2),
+    fPos: vec(-1,2),
+    gPos: vec(5,2),
+    hPos: vec(9,2)
+}
 
 options = {
     viewSize: {x: G.WIDTH, y:G.HEIGHT},
@@ -16,6 +90,16 @@ options = {
 function update() {
     if (!ticks) {
     }
+
+    //render logo based based on dvdLogo.pos
+    char('a',dvdLogo.pos.x+dvdLogo.aPos.x,dvdLogo.pos.y+dvdLogo.aPos.y);
+    char('b',dvdLogo.pos.x+dvdLogo.bPos.x,dvdLogo.pos.y+dvdLogo.bPos.y);
+    char('c',dvdLogo.pos.x+dvdLogo.cPos.x,dvdLogo.pos.y+dvdLogo.cPos.y);
+    char('d',dvdLogo.pos.x+dvdLogo.dPos.x,dvdLogo.pos.y+dvdLogo.dPos.y);
+    char('e',dvdLogo.pos.x+dvdLogo.ePos.x,dvdLogo.pos.y+dvdLogo.ePos.y);
+    char('f',dvdLogo.pos.x+dvdLogo.fPos.x,dvdLogo.pos.y+dvdLogo.fPos.y);
+    char('g',dvdLogo.pos.x+dvdLogo.gPos.x,dvdLogo.pos.y+dvdLogo.gPos.y);
+    char('h',dvdLogo.pos.x+dvdLogo.hPos.x,dvdLogo.pos.y+dvdLogo.hPos.y);
 }
 
 addEventListener("load", onLoad);
