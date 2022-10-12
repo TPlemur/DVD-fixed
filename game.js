c90c58126d39d20b9e91ac322f559fad10259a24
TPlemur <TPlemur@gmail.com>
2022-10-12 14:14:06 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,21 @@
+title = "FooBar";
+
+description = ``;
+
+characters = [];
+
+const G ={
+   WIDTH: 150,
+   HEIGHT: 150, 
+};
+
+options = {
+    viewSize: {x: G.WIDTH, y:G.HEIGHT},
+};
+
+function update() {
+    if (!ticks) {
+    }
+}
+
+addEventListener("load", onLoad);
